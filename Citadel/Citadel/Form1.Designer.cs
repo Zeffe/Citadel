041988d42e433730d94cf1019a76da8dcff65eb1
133spider <dragonfable_1379@yahoo.com>
2015-12-28 20:46:37 -0800

    added theme

--- Citadel/Citadel/Form1.Designer.cs
+++ Citadel/Citadel/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.thirteenForm1 = new asset.ThirteenForm();
+            this.thirteenControlBox1 = new asset.ThirteenControlBox();
+            this.thirteenTextBox1 = new asset.ThirteenTextBox();
+            this.thirteenTextBox2 = new asset.ThirteenTextBox();
+            this.thirteenForm1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // thirteenForm1
+            // 
+            this.thirteenForm1.AccentColor = System.Drawing.Color.DodgerBlue;
+            this.thirteenForm1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenForm1.ColorScheme = asset.ThirteenForm.ColorSchemes.Dark;
+            this.thirteenForm1.Controls.Add(this.thirteenTextBox2);
+            this.thirteenForm1.Controls.Add(this.thirteenTextBox1);
+            this.thirteenForm1.Controls.Add(this.thirteenControlBox1);
+            this.thirteenForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.thirteenForm1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.thirteenForm1.ForeColor = System.Drawing.Color.White;
+            this.thirteenForm1.Location = new System.Drawing.Point(0, 0);
+            this.thirteenForm1.Name = "thirteenForm1";
+            this.thirteenForm1.Size = new System.Drawing.Size(221, 212);
+            this.thirteenForm1.TabIndex = 0;
+            this.thirteenForm1.Text = "Citadel - [ Login ]";
+            // 
+            // thirteenControlBox1
+            // 
+            this.thirteenControlBox1.AccentColor = System.Drawing.Color.DodgerBlue;
+            this.thirteenControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.thirteenControlBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenControlBox1.ColorScheme = asset.ThirteenControlBox.ColorSchemes.Dark;
+            this.thirteenControlBox1.ForeColor = System.Drawing.Color.White;
+            this.thirteenControlBox1.Location = new System.Drawing.Point(118, 3);
+            this.thirteenControlBox1.Name = "thirteenControlBox1";
+            this.thirteenControlBox1.Size = new System.Drawing.Size(100, 25);
+            this.thirteenControlBox1.TabIndex = 0;
+            this.thirteenControlBox1.Text = "thirteenControlBox1";
+            // 
+            // thirteenTextBox1
+            // 
+            this.thirteenTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.thirteenTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.thirteenTextBox1.ColorScheme = asset.ThirteenTextBox.ColorSchemes.Dark;
+            this.thirteenTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.thirteenTextBox1.ForeColor = System.Drawing.Color.White;
+            this.thirteenTextBox1.Location = new System.Drawing.Point(47, 61);
+            this.thirteenTextBox1.Name = "thirteenTextBox1";
+            this.thirteenTextBox1.Size = new System.Drawing.Size(100, 22);
+            this.thirteenTextBox1.TabIndex = 1;
+            // 
+            // thirteenTextBox2
+            // 
+            this.thirteenTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.thirteenTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.thirteenTextBox2.ColorScheme = asset.ThirteenTextBox.ColorSchemes.Dark;
+            this.thirteenTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.thirteenTextBox2.ForeColor = System.Drawing.Color.White;
+            this.thirteenTextBox2.Location = new System.Drawing.Point(47, 89);
+            this.thirteenTextBox2.Name = "thirteenTextBox2";
+            this.thirteenTextBox2.Size = new System.Drawing.Size(100, 22);
+            this.thirteenTextBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 212);
+            this.Controls.Add(this.thirteenForm1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.thirteenForm1.ResumeLayout(false);
+            this.thirteenForm1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private asset.ThirteenForm thirteenForm1;
+        private asset.ThirteenControlBox thirteenControlBox1;
+        private asset.ThirteenTextBox thirteenTextBox2;
+        private asset.ThirteenTextBox thirteenTextBox1;
     }
 }
 
