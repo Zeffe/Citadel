dca042da96820cf0e85e2f1ef21270798b549c8b
Zeffer <zeffer5789@gmail.com>
2016-01-28 12:42:19 -0600

    Testing extensions

---
+++ Citadel/Citadel/extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class extensions
+    {
+        public static void Sort(this int[] arr)
+        {
+            int temp = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i + 1] < arr[i])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+}
